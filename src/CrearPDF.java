84e1cf7360dc86b28604f7f930886e6e20554b1b
Christian <chralmins@alu.edu.gvs.es>
2025-10-07 14:55:40 +0200

    Cambio mensaje crear pdf

--- src/CrearPDF.java
+++ src/CrearPDF.java
@@ -8,8 +8,7 @@ public class CrearPDF {
         Document document = new Document();
         String parrafo = "Hola me llamo Christian Almeida y este es mi primer PDF, "
                 + "me ha costado un poco, "
-                +"pero lo he conseguido."
-                +"Estoy probando con el git";
+                +"pero lo he conseguido.";
         PdfWriter.getInstance(document, new FileOutputStream("MiprimerPDF.pdf"));
         document.open();
         document.add(new Paragraph(parrafo));
